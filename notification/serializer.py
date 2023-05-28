50151fb5b5e74c788763fa2cf80d7eabd30cd7d2
par <par@localhost.localdomain>
2023-05-28 15:57:07 +0300

    permission

---
+++ notification/serializer.py
@@ -0,0 +1,5 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,25 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.core.mail import send_mail
+import math, random
+from django.urls import reverse
+from django.contrib.sites.shortcuts import get_current_site
+from rest_framework.decorators import api_view,permission_classes
+from django.contrib.auth.decorators import login_required
+from drf_multiple_model.pagination import MultipleModelLimitOffsetPagination
+from rest_framework.response import Response
+import json
+import jwt
+from django.conf import settings
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status, permissions
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import make_password
+from drf_multiple_model.views import FlatMultipleModelAPIView
+from .serializer import lostpSerializer,lostiSerializer,foundiSerializer,foundpSerializer,accountSerializer,registerserializer,loginSerializer,wantedpSerializer,changepserializer,changeaccountserializer
+from .models import lost_P,found_P,lost_i,found_i,account,person_type,item_type,wanted_p
+from drf_multiple_model.views import ObjectMultipleModelAPIView
+from django.contrib.auth import authenticate,login,logout
+from rest_framework.exceptions import AuthenticationFailed
